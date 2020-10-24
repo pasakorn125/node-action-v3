08e758420d81adba56d4cfddba57f240ea4d2c89
pasakorn125 <60160304@go.buu.ac.th>
2020-10-24 16:03:58 +0700

    Rebuild v2

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+
 app.get('/', (req, res) => {
-    return res.send('Hello')
+    return res.send('Hello!')
 })
 
 app.listen(port, () => {
     console.log(`Start Express application at ${port}`)
+})
+app.get('/item', (req, res) => {
+    return res.send('items')
 })
 
 module.exports = app
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,33 @@
 const assert = require('assert')
+const request = require('supertest')
+const app = require('../index')
 
 describe('Validate setting the test', () => {
     it('should be ok', () => {
-        assert.ok(false)
+        assert.ok(true)
     })
+})
+
+describe('GET "/" ' ,() => {
+    it('respond with Hello!', (done) => {
+        request(app).get('/').expect('Hello!',done)
+
+
+
+    })
+
+
+})
+
+describe('GET "/item" ' ,() => {
+    it('respond array of items', (done) => {
+        let want = [{id: 1, name: 'Iphone12 pro max'
+        ,id: 2, name: 'nokia 3310'}]
+        request(app).get('/item').expect('items',done)
+
+
+
+    })
+
+
 })
